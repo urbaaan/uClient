d875a418565546b2296315b794941f0d87291cc1
urbaaan <71067316+urbaaan@users.noreply.github.com>
2021-02-05 17:45:38 +0800

    made it into class instead

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 /* eslint-disable node/no-callback-literal */
 /* eslint-disable node/no-path-concat */
 
@@ -7,161 +8,179 @@ const {
   BrowserWindow,
   clipboard,
   protocol,
-  ipcMain,
-  session
+  ipcMain
 } = require('electron')
 
-console.log('Starting...')
-const DEVELOPMENT = true
-
-if (DEVELOPMENT === false) {
-  const process = require('process')
-  const { spawn } = require('child_process')
-
-  if (process.env.GPUSET !== 'true') {
-    spawn(process.execPath, process.argv, {
-      env: {
-        ...process.env,
-        SHIM_MCCOMPAT: '0x800000001',
-        GPUSET: true
-      },
-      detached: true
-    })
-    process.exit(0)
-  }
-}
-const DEFAULT_CONFIG = {
-  capFPS: false,
-  RPC: true,
-  test: 0,
-  test2: 1,
-  tet: 1,
-  a: 1,
-  disableHands: true,
-  ADS: false,
-  hitMarkers: true,
-  dimensions: {
-    fullscreen: true,
-    maximised: true,
-    size: {
-      width: 1920,
-      height: 1080,
-      x: 0,
-      y: 0
-    }
-  }
-}
-const { autoUpdater } = require('electron-updater')
 const config = new (require('electron-store'))()
 
-Object.keys(DEFAULT_CONFIG).forEach((keys) => {
-  if (!config.get(keys)) {
-    config.set(keys, DEFAULT_CONFIG[keys])
-  }
-})
-// RPC
-console.log('Starting Discord Client...')
-const discord = new (require('discord-rpc').Client)({
-  transport: 'ipc'
-})
-if (config.get('RPC') === true) {
-  discord
-    .login({
-      clientId: '769550318148780115'
-    })
-    .catch((error) => console.log(error))
-}
-console.log('Discord Client Running!')
-
-const prompt = require('electron-prompt')
-const { normalize } = require('path')
-
-app.commandLine.appendSwitch('disable-http-cache')
-if (config.get('capFPS') === false) {
-  app.commandLine.appendSwitch('disable-frame-rate-limit')
-  app.commandLine.appendSwitch('disable-gpu-vsync')
-}
-
-function createGameWindow () {
-  console.log('Creating Window...')
-  const gameWindow = new BrowserWindow({
-    width: config.get('dimensions.size.width'),
-    height: config.get('dimensions.size.height'),
-    fullscreen: config.get('dimensions.fullscreen'),
-    x: config.get('dimensions.size.x'),
-    y: config.get('dimensions.size.y'),
-    show: false,
-    title: 'uClient',
-    webPreferences: {
-      preload: `${__dirname}/preload/game.js`,
-      v8CacheOptions: 'bypassHeatCheckAndEagerCompile'
+class Start {
+  constructor () {
+    app.commandLine.appendSwitch('disable-http-cache')
+    if (!config.get('capFPS')) {
+      app.commandLine.appendSwitch('disable-frame-rate-limit')
+      app.commandLine.appendSwitch('disable-gpu-vsync')
     }
-  })
-  gameWindow.removeMenu()
-  gameWindow.maximize(config.get('dimensions.maximised'))
-  console.log('Creation of Window Successful!')
 
-  const { webContents } = gameWindow
+    const { normalize } = require('path')
+    app.whenReady()
+      .then(() => {
+        protocol.registerFileProtocol('swap', (request, callback) => {
+          callback({
+            path: normalize(request.url.replace(/^swap:/, ''))
+          })
+        })
+        if (app.requestSingleInstanceLock()) {
+          this.startConfig()
+          this.startRPC()
+          this.createWindow('https://venge.io')
+        } else console.log('ERROR: More than one Application opened...')
+      })
+      .catch((error) => console.log(error))
+  }
 
-  // This purely creates the Venge.io Window and ensures the game works.
-  gameWindow.loadURL('https://venge.io')
-  console.log('Loading Content...')
+  registerShortcut () {
+    this.shortcut = require('electron-localshortcut')
+    this.shortcut.register(this.gameWindow, 'F1', () => {
+      this.gameWindow.loadURL('https://venge.io')
+    })
+    this.shortcut.register(this.gameWindow, 'F2', () => {
+      if (
+        new URL(clipboard.readText()).hostname === 'venge.io' &&
+        new URL(clipboard.readText()).pathname.charAt(0) === '#'
+      ) {
+        console.log(clipboard.readText())
+        this.gameWindow.loadURL(clipboard.readText())
+      } else {
+        getLink()
+      }
 
-  console.log('Fix Caching...')
-  session.fromPartition('persist:name', { cache: false })
+      function getLink () {
+        (require('electron-prompt'))({
+          title: 'Play',
+          label: 'Enter URL:',
+          alwaysOnTop: true,
+          type: 'input',
+          inputAttrs: {
+            type: 'url',
+            required: true,
+            placeholder: 'https://venge.io/#'
+          },
+          resizable: false,
+          height: 200,
+          value: 'https://venge.io/#'
+        }).then((r) => {
+          const a = r.split('#')
+          if (
+            r.includes('https://venge.io/#') &&
+            a.length > 1 &&
+            a[1].length > 4
+          ) {
+            this.gameWindow.loadURL(r)
+          } else {
+            getLink()
+          }
+        })
+      }
+    })
+    this.shortcut.register(this.gameWindow, 'F3', () => {
+      this.gameWindow.webContents
+        .executeJavaScript(
+          'pc.app.fire("Chat:Message", "uClient", "Link copied!");'
+        )
+        .then(() => clipboard.writeText(this.gameWindow.webContents.getURL()))
+        .catch((error) => console.log(error))
+    })
+    this.shortcut.register(this.gameWindow, 'F11', () => {
+      this.gameWindow.setSimpleFullScreen(!this.gameWindow.isSimpleFullScreen())
+    })
+    this.shortcut.register(this.gameWindow, 'F12', () => {
+      this.gameWindow.webContents.openDevTools({
+        mode: 'right'
+      })
+    })
+    this.shortcut.register(this.gameWindow, 'CommandOrControl+F5', () => {
+      this.gameWindow.webContents.reloadIgnoringCache()
+    })
+    this.shortcut.register('F10', () => {
+      this.gameWindow.minimize(!this.gameWindow.isMinimized())
+    })
+    this.shortcut.register(this.gameWindow, 'Esc', () => {
+      this.gameWindow.webContents.executeJavaScript(
+        'document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;document.exitPointerLock();'
+      )
+    })
+    this.shortcut.register('Alt+F4', () => {
+      app.quit()
+    })
+  }
 
-  // Listening to Events.
-  console.log('Initializing Listeners...')
-  gameWindow.on('page-title-updated', (event) => event.preventDefault())
-  gameWindow.on('ready-to-show', () => {
-    gameWindow.show()
-    app.focus()
-  })
-  gameWindow.on('resize', () => {
-    config.set('dimensions.size', gameWindow.getBounds())
-    config.set('dimensions.fullscreen', gameWindow.isFullScreen())
-    config.set('dimensions.maximised', gameWindow.isMaximized())
-  })
+  createWindow (url) {
+    this.gameWindow = new BrowserWindow({
+      width: config.get('dimensions.size.width'),
+      height: config.get('dimensions.size.height'),
+      fullscreen: config.get('dimensions.fullscreen'),
+      show: false,
+      x: config.get('dimensions.size.x'),
+      y: config.get('dimensions.size.y'),
+      webPreferences: {
+        preload: `${__dirname}/preload/game.js`
+      }
+    })
 
-  // Configuration
-  webContents.on('will-prevent-unload', (event) => event.preventDefault())
-  webContents.on('dom-ready', () => {
-    // Autoupdater
+    this.gameWindow.removeMenu()
+    this.gameWindow.maximize(config.get('dimensions.maximised'))
+    this.gameWindow.loadURL(url)
+    this.setActivity(0)
+
+    ipcMain.on('RPC', (event, json) => {
+      this.setActivity(JSON.parse(json))
+    })
+
+    this.registerShortcut()
+    this.gameWindow.on('page-title-updated', (event) => event.preventDefault())
+    this.gameWindow.on('ready-to-show', () => {
+      this.gameWindow.show()
+      app.focus()
+    })
+    this.gameWindow.on('resize', () => {
+      config.set('dimensions.size', this.gameWindow.getBounds())
+      config.set('dimensions.fullscreen', this.gameWindow.isFullScreen())
+      config.set('dimensions.maximised', this.gameWindow.isMaximized())
+    })
+
+    this.gameWindow.webContents.on('will-prevent-unload', (event) => event.preventDefault())
+    this.gameWindow.webContents.on('dom-ready', () => {
+      this.startUpdater()
+      this.startSwapper()
+      this.createSettings()
+    })
+    this.gameWindow.webContents.on('new-window', (event, url) => {
+      if (!url.includes('venge.io')) {
+        event.preventDefault()
+        shell.openExternal(url)
+      }
+    })
+  }
+
+  startUpdater () {
+    const { autoUpdater } = require('electron-updater')
     autoUpdater.checkForUpdatesAndNotify()
 
     autoUpdater.once('update-available', () => {
-      webContents.executeJavaScript(
+      this.gameWindow.webContents.executeJavaScript(
         'alert("Update is available and will be installed in the background.")'
       )
     })
 
     autoUpdater.once('update-downloaded', () => {
-      webContents
+      this.gameWindow.webContents
         .executeJavaScript('alert("The latest update will be installed now.")')
         .then(() => autoUpdater.quitAndInstall())
         .catch((error) => console.log(error))
     })
+  }
 
-    function setActivity (value) {
-      discord.setActivity({
-        largeImageKey: 'logo',
-        largeImageText: value.username || 'uClient',
-        endTimestamp: new Date().setTime(Date.now() + value.time) || undefined,
-        state: value.map || undefined,
-        details: value.mode || 'In Menu'
-      })
-    }
-
-    setActivity(0) // Sets RPC to Default.
-
-    ipcMain.on('RPC', (event, json) => {
-      setActivity(JSON.parse(json))
-    })
-
-    // Settings
-    createSettingsWindow()
-
-    // Resource Swapper
+  startSwapper () {
     const { readdirSync, mkdir, statSync } = require('fs')
 
     const swapFolder = `${app.getPath('documents')}/uClientSwapper`
@@ -198,7 +217,7 @@ function createGameWindow () {
     resourceSwap(swapFolder)
 
     if (swap.filter.urls.length > 0) {
-      webContents.session.webRequest.onBeforeRequest(
+      this.gameWindow.webContents.session.webRequest.onBeforeRequest(
         swap.filter,
         (details, callback) => {
           console.log(
@@ -216,91 +235,60 @@ function createGameWindow () {
         }
       )
     }
-  })
+  }
 
-  webContents.on('new-window', (event, url) => {
-    if (new URL(url).hostname !== 'venge.io') {
-      event.preventDefault()
-      shell.openExternal(url)
-    }
-  })
-
-  // Shortcuts
-  const shortcut = require('electron-localshortcut')
-  shortcut.register(gameWindow, 'F1', () => {
-    gameWindow.loadURL('https://venge.io')
-  })
-  shortcut.register(gameWindow, 'F2', () => {
-    if (
-      new URL(clipboard.readText()).hostname === 'venge.io' &&
-      new URL(clipboard.readText()).pathname.charAt(0) === '#'
-    ) {
-      gameWindow.loadURL(clipboard.readText())
-    } else {
-      getLink()
-    }
-
-    function getLink () {
-      prompt({
-        title: 'Play',
-        label: 'Enter URL:',
-        alwaysOnTop: true,
-        type: 'input',
-        inputAttrs: {
-          type: 'url',
-          required: true,
-          placeholder: 'https://venge.io/#'
-        },
-        resizable: false,
-        height: 200,
-        value: 'https://venge.io/#'
-      }).then((r) => {
-        const a = r.split('#')
-        if (
-          r.includes('https://venge.io/#') &&
-          a.length > 1 &&
-          a[1].length > 4
-        ) {
-          gameWindow.loadURL(r)
-        } else {
-          getLink()
+  startConfig () {
+    const DEFAULT_CONFIG = {
+      capFPS: false,
+      RPC: true,
+      disableHands: true,
+      ADS: false,
+      hitMarkers: true,
+      dimensions: {
+        fullscreen: true,
+        maximised: true,
+        size: {
+          width: 1920,
+          height: 1080,
+          x: 0,
+          y: 0
         }
-      })
+      }
     }
-  })
-  shortcut.register(gameWindow, 'F3', () => {
-    webContents
-      .executeJavaScript(
-        'pc.app.fire("Chat:Message", "uClient", "Link copied!");'
-      )
-      .then(() => clipboard.writeText(webContents.getURL()))
-      .catch((error) => console.log(error))
-  })
-  shortcut.register(gameWindow, 'F11', () => {
-    gameWindow.setSimpleFullScreen(!gameWindow.isSimpleFullScreen())
-  })
-  shortcut.register(gameWindow, 'F12', () => {
-    webContents.openDevTools({
-      mode: 'right'
-    })
-  })
-  shortcut.register(gameWindow, 'CommandOrControl+F5', () => {
-    webContents.reloadIgnoringCache()
-  })
-  shortcut.register(gameWindow, 'F10', () => {
-    gameWindow.maximize(!gameWindow.isMaximized())
-  })
-  shortcut.register(gameWindow, 'Esc', () => {
-    webContents.executeJavaScript(
-      'document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;document.exitPointerLock();'
-    )
-  })
-  shortcut.register('Alt+F4', () => {
-    app.quit()
-  })
 
-  function createSettingsWindow () {
-    const settings = new BrowserWindow({
+    Object.keys(DEFAULT_CONFIG).forEach((keys) => {
+      if (!config.get(keys)) {
+        config.set(keys, DEFAULT_CONFIG[keys])
+      }
+    })
+  }
+
+  startRPC () {
+    this.discord = new (require('discord-rpc').Client)({
+      transport: 'ipc'
+    })
+    if (config.get('RPC')) {
+      this.discord
+        .login({
+          clientId: '769550318148780115'
+        })
+        .catch((error) => console.log(error))
+      console.log('RPC Activated!')
+    }
+  }
+
+  setActivity (value) {
+    this.discord.setActivity({
+      largeImageKey: 'logo',
+      largeImageText: value.username || 'uClient',
+      endTimestamp: new Date().setTime(Date.now() + value.time) || undefined,
+      state: value.map || undefined,
+      details: value.mode || 'In Menu'
+    })
+  }
+
+  createSettings () {
+    this.settings = new BrowserWindow({
       width: 1440 / 2,
       height: 900 / 2,
       center: true,
@@ -309,22 +297,23 @@ function createGameWindow () {
       resizable: false,
       fullscreenable: false,
       modal: true,
-      parent: gameWindow,
+      parent: this.gameWindow,
       webPreferences: {
         nodeIntegration: true,
         enableRemoteModule: true
       }
     })
-    settings.loadURL(`file://${__dirname}/html/settings.html`)
-    shortcut.register('=', () => {
-      if (settings.isVisible()) {
-        settings.hide()
+
+    this.settings.loadURL(`file://${__dirname}/html/settings.html`)
+    this.shortcut.register('=', () => {
+      if (this.settings.isVisible()) {
+        this.settings.hide()
         app.focus()
-        gameWindow.focus()
-      } else settings.show()
+        this.gameWindow.focus()
+      } else this.settings.show()
     })
-    shortcut.register(settings, 'F12', () => {
-      settings.openDevTools()
+    this.shortcut.register(this.settings, 'F12', () => {
+      this.settings.openDevTools()
     })
 
     ipcMain.on('relaunch', () => {
@@ -333,9 +322,6 @@ function createGameWindow () {
     })
   }
 }
-app.on('window-all-closed', () => {
-  app.quit()
-})
 
 protocol.registerSchemesAsPrivileged([
   {
@@ -347,12 +333,4 @@ protocol.registerSchemesAsPrivileged([
   }
 ])
 
-app.whenReady().then(() => {
-  protocol.registerFileProtocol('swap', (request, callback) => {
-    callback({
-      path: normalize(request.url.replace(/^swap:/, ''))
-    })
-  })
-  if (app.requestSingleInstanceLock()) createGameWindow()
-  else console.log('ERROR: More than one Application opened...')
-})
+new Start()
